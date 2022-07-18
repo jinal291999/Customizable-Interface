c282dcc1f1dfe4502fda8e5fafbc1c5e0929dc17
JinalPatel <83238550+jinal291999@users.noreply.github.com>
2022-07-18 00:09:36 -0400

    Add files via upload

---
+++ Customizable Interface/lab9.js
@@ -0,0 +1,31 @@
+﻿//LAB 9-DATA STORAGE: INDEX PAGE
+// alert("home");//DELETE AFTER CONFIRMING CONNECTION
+window.onload = function () {
+
+    var username = localStorage.getItem("name");  //get name
+    var newcolor = localStorage.getItem("incolor");
+    var userout = document.getElementById("newMsgBox");  
+    var colour = document.getElementById("f_color");
+    // console.log(username, newcolor);    
+    userout.innerText = "Welcome! " + username;
+    document.body.style.background = newcolor;
+    var formhandle = document.forms.infoForm;   
+    function processform() {                      //Function to handle form on submit
+        var fname = formhandle.f_name;
+        if (fname.value != "") {
+            localStorage.setItem("name", fname.value);
+        }
+        var ncolour = formhandle.f_color;
+        if (ncolour.value != "") {
+            localStorage.setItem("incolor", ncolour.value);
+        }
+    }
+    var btnDel = document.getElementById("btnDel");   
+    function removedata() {                   // Function to delete the localstorage
+        localStorage.clear();
+        location.reload();
+    }
+    formhandle.onsubmit = processform;
+    btnDel.onclick = removedata;
+
+}
---
+++ Customizable Interface/products.js
@@ -0,0 +1,11 @@
+﻿//LAB 9-DATA STORAGE: PRODUCTS PAGE
+window.onload = function () {       //Function to get name and color in products
+
+    var username = localStorage.getItem("name");
+    var newcolor = localStorage.getItem("incolor");
+    var userout = document.getElementById("newMsgBox");
+    var colour = document.getElementById("f_color");
+    console.log(username, newcolor);
+    userout.innerText = "Welcome! " + username;
+    document.body.style.background = newcolor;
+}
